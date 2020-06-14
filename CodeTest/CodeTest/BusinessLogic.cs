29a42eb2f4d40a4c17495fbcfc87246deca23ec4
maheswar <g_maheswar@rediffmail.com>
2020-06-14 10:49:05 +0530

    Added code
    added code

---
+++ CodeTest/CodeTest/BusinessLogic.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    interface BusinessLogic
+    {
+        string GetBusinessRuleLogic();
+    }
+
+    public class PhysicalProduct : BusinessLogic
+    {
+        //write a logic for, packing slip for shipping
+        public string GetBusinessRuleLogic()
+        {
+            return "packing slip for shipping";
+        }
+    }
+
+    public class Book : BusinessLogic
+    {
+        //write a logic for, create a duplicate packing slip for royality department
+        public string GetBusinessRuleLogic()
+        {
+            return "create a duplicate packing slip for royality department";
+        }
+    }
+
+    public class Membership : BusinessLogic
+    {
+        //write a logic for, Activate the Membership
+        public string GetBusinessRuleLogic()
+        {
+            return "Activate the Membership";
+        }
+    }
+
+    public class Upgrade : BusinessLogic
+    {
+        //write a logic for, Applying for Upgrade
+        public string GetBusinessRuleLogic()
+        {
+            return "Applying for Upgrade";
+        }
+    }
+
+    public class MembershipOrUpgrade : BusinessLogic
+    {
+        //write a logic for, email to the owner and inform them of activation / Upgrade
+        public string GetBusinessRuleLogic()
+        {
+            return "email to the owner and inform them of activation / Upgrade";
+        }
+    }
+
+    public class LearningToSKI : BusinessLogic
+    {
+        //write a logic for, Add a first aid video to the packin slip
+        public string GetBusinessRuleLogic()
+        {
+            return "Add a first aid video to the packin slip";
+        }
+    }
+
+    public class PhysicalProductOrABook : BusinessLogic
+    {
+        //write a logic for, Commission payment to the agent
+        public string GetBusinessRuleLogic()
+        {
+            return "Commission payment to the agent";
+        }
+    }
+
+}
---
+++ CodeTest/CodeTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessLogic physicalProduct = new PhysicalProduct();
+            BusinessLogic book = new Book();
+            BusinessLogic membership = new Membership();
+            BusinessLogic upgrade = new Upgrade();
+            BusinessLogic membershipOrUpgrade = new MembershipOrUpgrade();
+            BusinessLogic learningToSKI = new LearningToSKI();
+            BusinessLogic physicalProductOrABook = new PhysicalProductOrABook();
+
+            string physicalProductString = physicalProduct.GetBusinessRuleLogic();
+            string bookString = book.GetBusinessRuleLogic();
+            string membershipString = membership.GetBusinessRuleLogic();
+            string upgradeString = upgrade.GetBusinessRuleLogic();
+            string membershipOrUpgradeString = membershipOrUpgrade.GetBusinessRuleLogic();
+            string learningToSKIString = learningToSKI.GetBusinessRuleLogic();
+            string physicalProductOrABookString = physicalProductOrABook.GetBusinessRuleLogic();
+
+            Console.WriteLine(physicalProductString);
+            Console.WriteLine(bookString);
+            Console.WriteLine(membershipString);
+            Console.WriteLine(upgradeString);
+            Console.WriteLine(membershipOrUpgradeString);
+            Console.WriteLine(learningToSKIString);
+            Console.WriteLine(physicalProductOrABookString);
+            Console.ReadKey();
+        }
+    }
+}
